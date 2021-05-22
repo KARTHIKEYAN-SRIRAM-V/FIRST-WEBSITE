dccc1605b2c767ab713b3187341d96f6004428f0
KARTHIKEYAN-SRIRAM-V <karthikeyansriramv@gmail.com>
2021-05-22 20:18:27 +0530

    MAINFOLDER

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -20,12 +20,8 @@ from django.views.static import serve
 from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/clearcache/', include('clearcache.urls')),
     path('admin/', admin.site.urls),
     path("projects/", include("projects.urls")),
 
- url(r'^media/(?P<path>.*)$', serve,{'document_root':       
-settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': 
-settings.STATIC_ROOT}), 
+   
 ]
